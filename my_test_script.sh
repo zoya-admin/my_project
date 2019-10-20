6f81de8c4800c6c873a4abf69df1339768be8f62
zoya-admin <zoyabogomolova@gmail.com>
2019-10-20 16:19:24 +0300

    add script

---
+++ my_test_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo '================================= script is starting ====================================='
+
+mkdir tmp_directory
+cd tmp_directory
+
+tee new_file.txt <<EOF
+This is my new file
+Im sitting in subway with Roma
+EOF
+
+ll > fgr.txt
+
